d0ac0b949ed5eafc4a91728e15e24947784210bc
Dario Vincenzo De Simone <dariovincenzo.desimone@gmail.com>
2024-02-08 10:40:24 +0100

    build for github

--- docs/assets/environments/environment.ts
+++ docs/assets/environments/environment.ts
@@ -1,1 +1,5 @@
-export const magickey = 'pk_live_E3F46F492F8EB8BD';
+export const magicKey = 'pk_live_B833DA9672005704';
+export const network = {
+    rpcUrl: 'https://data-seed-prebsc-1-s1.binance.org:8545/',
+    chainId: 97
+}
