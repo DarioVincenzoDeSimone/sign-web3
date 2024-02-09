2a2c1d17e7fbd134d95f55815276f963c42e5f58
Dario Vincenzo De Simone <dariovincenzo.desimone@gmail.com>
2024-02-09 10:40:16 +0100

    home draft

--- src/app/pages/user/home/home.component.ts
+++ src/app/pages/user/home/home.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { from, switchMap } from 'rxjs';
+import { SmartContractService } from 'src/services/smart-contract.service';
+import { IUser, UserService } from 'src/services/user.service';
+import { Web3Service } from 'src/services/web3.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+  userInfo: IUser;
+
+  constructor(
+    private web3Service: Web3Service,
+    private smartContractService: SmartContractService,
+    private userService: UserService
+  ) { }
+
+
+  async ngOnInit(): Promise<void> {
+    this.userService.userInfo$.pipe(switchMap(user => {
+      this.userInfo = user;
+      return from(this.smartContractService.maialettoContract.methods['getRecordByAddress'](user.wallet).call())
+    })).subscribe(data => console.log('data da chain', data));
+  }
 
 }
