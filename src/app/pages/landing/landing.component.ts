a6fa58b35e0f6f5f5088e6e2862fe22c50df09f4
Dario Vincenzo De Simone <dariovincenzo.desimone@gmail.com>
2024-02-16 21:48:31 +0100

    improved landing page

--- src/app/pages/landing/landing.component.ts
+++ src/app/pages/landing/landing.component.ts
@@ -8,31 +8,31 @@ import { DomSanitizer } from '@angular/platform-browser';
 })
 export class LandingComponent {
 
-  currentPosition: any;
+  // currentPosition: any;
 
-  constructor(private _sanitizer: DomSanitizer) { }
+  // constructor(private _sanitizer: DomSanitizer) { }
 
-  getSVGImage(image) {
-    return this._sanitizer.bypassSecurityTrustHtml(`${image}`);
-  }
+  // getSVGImage(image) {
+  //   return this._sanitizer.bypassSecurityTrustHtml(`${image}`);
+  // }
 
-  @HostListener('window:scroll', ['$event'])
-  onScroll(event) {
-    // let scroll = window.scrollY;
-    // console.log(event)
-    // document.getElementById('wallpaper-container').scrollBy({ top: scroll / 35 })
-    let scroll = window.scrollY;
-    let el = document.getElementById('wallpaper-container') as HTMLElement;
-    if (scroll > this.currentPosition && el.getBoundingClientRect().bottom >= 0) {
-      // console.log('scrollDown');
-      el.scrollBy({ top: scroll / 35 })
-    } else if (scroll <= this.currentPosition && el.getBoundingClientRect().bottom >= 0) {
-      // console.log('scrollUp');
-      el.scrollBy({ top: -scroll / 35 })
-    } else if (el.getBoundingClientRect().bottom < 0) {
-      el.scrollTo({ top: el.getBoundingClientRect().height });
-    }
-    this.currentPosition = scroll;
-  }
+  // @HostListener('window:scroll', ['$event'])
+  // onScroll(event) {
+  //   // let scroll = window.scrollY;
+  //   // console.log(event)
+  //   // document.getElementById('wallpaper-container').scrollBy({ top: scroll / 35 })
+  //   let scroll = window.scrollY;
+  //   let el = document.getElementById('wallpaper-container') as HTMLElement;
+  //   if (scroll > this.currentPosition && el.getBoundingClientRect().bottom >= 0) {
+  //     // console.log('scrollDown');
+  //     el.scrollBy({ top: scroll / 35 })
+  //   } else if (scroll <= this.currentPosition && el.getBoundingClientRect().bottom >= 0) {
+  //     // console.log('scrollUp');
+  //     el.scrollBy({ top: -scroll / 35 })
+  //   } else if (el.getBoundingClientRect().bottom < 0) {
+  //     el.scrollTo({ top: el.getBoundingClientRect().height });
+  //   }
+  //   this.currentPosition = scroll;
+  // }
 
 }
